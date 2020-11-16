7325c76e1bc959d2d94d95e7f5dea85a04619ee3
Tomislav Trbic <31571445+trba1810@users.noreply.github.com>
2020-11-16 13:20:54 +0100

    initial commit

---
+++ src/Crawler.php
@@ -0,0 +1,21 @@
+<?php 
+namespace Practice2020\Crawler;
+
+
+class Crawler
+{
+    private $data = 5;
+
+    public function __construct()
+    {
+        
+    }
+
+    public function get($data)
+    {
+        return $this->data;
+    }
+}
+
+
+
---
+++ tests/CrawlerTest.php
@@ -0,0 +1,12 @@
+<?php 
+
+use Practice2020\Crawler;
+
+class CrawlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTrueAssertsToTrue()
+    {
+        $this->assertTrue(true);
+    }
+
+}
