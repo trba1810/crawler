861d94d910891093b075ccd80388de52901f503a
Tomislav Trbic <31571445+trba1810@users.noreply.github.com>
2021-02-05 21:28:18 +0100

    changes to bulk

--- src/Crawler.php
+++ src/Crawler.php
@@ -2,19 +2,41 @@
 namespace Practice2020\Crawler;
 
 use HeadlessChromium\BrowserFactory;
+use Exception;
 
 class Crawler
 {
-    // private $data = 5;
+    private $data = [];
+    public $errors = [];
+    public $bulk = [];
+    
 
     public function __construct()
     {
         
     }
 
-    public function get($data)
+    public function getData()
     {
         return $this->data;
+    }
+
+    public function getBulk()
+    {
+        return $this->bulk;
+    }
+
+    public function getConfig($config)
+    {
+        if(array_key_exists('price', $config)){
+            return $this->config;
+        }
+        return $config = [];
+    }
+
+    public function set($name, $value)
+    {
+       $this->data[$name] = $value; 
     }
 
     public function testPuppeteer()
@@ -23,26 +45,136 @@ class Crawler
 
         $browserFactory = new BrowserFactory('C:\Program Files (x86)\Google\Chrome\Application\chrome.exe');
     
-        // starts headless chrome
         $browser = $browserFactory->createBrowser();
     
-        // creates a new page and navigate to an url
         $page = $browser->createPage();
         $page->navigate('http://example.com')->waitForNavigation();
         
-        // get page title
         $pageTitle = $page->evaluate('document.title')->getReturnValue();
         
-        // screenshot - Say "Cheese"! 😄
         $page->screenshot()->saveToFile('./bar.png');
         
-        // pdf
+        
         $page->pdf(['printBackground'=>false])->saveToFile('./bar.pdf');
         
-        // bye
+        
         $browser->close();
     }
-}
 
+    public function crawl($url, $config = [])
+    {
+        $browserFactory = new BrowserFactory('C:\Program Files (x86)\Google\Chrome\Application\chrome.exe');
+    
+        $browser = $browserFactory->createBrowser();
+    
+        $page = $browser->createPage();
+
+        $page->navigate($url)->waitForNavigation();
+      
+        foreach ($config as $key => $value) {
+            
+            
+            try {
+                $this->data[$key] = $page->evaluate($value)->getReturnValue();
+                throw new Exception("Error on this key: " . $key);
+            } catch (Exception $e) {
+                echo $e->getMessage();
+                continue;
+            } 
+            
+            
+        }
+
+        $browser->close();
+
+        return $this->data;
+    }
+    
+    public function bulkImport($urls, $config = [])
+    {
+        $browserFactory = new BrowserFactory('C:\Program Files (x86)\Google\Chrome\Application\chrome.exe');
+    
+        $browser = $browserFactory->createBrowser();
+    
+        $page = $browser->createPage();
+
+        $page->navigate($urls)->waitForNavigation();
+        // foreach($urls as $url){
+        //     $page->navigate($url)->waitForNavigation();
+        // }
+
+        /**
+         * $outputs = [
+            [
+                "domain" => 'aksa',
+                "url" => 'www.aksa.rs',
+                "data" => [
+                "price" => 'document.querySelector("div.block.product-details-price > div.clearfix > div > span.product-price-value.value").innerText'
+                ]
+            ],
+            [
+                "domain" => 'winwin',
+                "url" => 'www.winwin.rs',
+                "data" => [
+                "price" => 'document.querySelector("div.block.product-details-price > div.clearfix > div > span.product-price-value.value").innerText'
+                ]
+            ],
+            
+        ];
+         */
+        $configs = [
+                [
+                    "domain" => 'aksa',
+                    "url" => 'www.aksa.rs',
+                    "data" => [
+                    "price" => 'document.querySelector("div.block.product-details-price > div.clearfix > div > span.product-price-value.value").innerText'
+                    ]
+                ],
+                [
+                    "domain" => 'winwin',
+                    "url" => 'www.winwin.rs',
+                    "data" => [
+                    "price" => 'document.querySelector("div.block.product-details-price > div.clearfix > div > span.product-price-value.value").innerText'
+                    ]
+                ],
+        
+            ];
+
+
+        foreach($configs as $config){
+            if(is_array($config)){
+                foreach($config as $conf){
+                    $bulk = $conf;
+                }
+            }
+            $bulk[] = $config;
+            print_r($bulk);
+            PHP_EOL;
+            // kako razdvojiti???
+        }
+        // foreach($config as $key => $value)
+        // {
+        //     try {
+        //         // kako da targetiram key?
+        //         foreach($value as $k => $info)
+        //         {
+        //             // $this->$data[$k] = $page->evaluate($info)->getReturnValue();
+                    
+        //             print_r($data[$k]);
+                    
+        //         }
+                
+        //         throw new Exception("Error on this key: " . $key);
+        //     } catch (Exception $e) {
+        //         echo $e->getMessage();
+        //         continue;
+        //     } 
+        // }
+        $browser->close();
+
+        return $this->bulk;
+    }
+    
+}
 
 
--- tests/CrawlerTest.php
+++ tests/CrawlerTest.php
@@ -5,17 +5,44 @@ use Practice2020\Crawler\Crawler;
 
 class CrawlerTest extends \PHPUnit_Framework_TestCase
 {
-    public function testTrueAssertsToTrue()
-    {
-        $this->assertTrue(true);
-    }
+    // public function testTrueAssertsToTrue()
+    // {
+    //     $this->assertTrue(true);
+    // }
 
-    public function testPup()
+    // public function testPup()
+    // {
+    //     $craw = new Crawler;
+    //     $craw->testPuppeteer();
+    //     $this->assertFileExists('./bar.pdf');
+    // }
+
+    // public function testCrawl()
+    // {
+    //     $craw = new Crawler;
+    //     $config = [
+    //         "price" => 'document.querySelector("div.block.product-details-price > div.clearfix > div > span.product-price-value.value").innerText', 
+    //     ];
+    //     $craw->crawl('https://www.aksa.rs/osnovni-modeli-kolica/1585-graco-kolica-evo',$config);
+    // }
+
+    public function testBulk()
     {
         $craw = new Crawler;
-        $craw->testPuppeteer();
-        $this->assertFileExists('/foo/bar.pdf');
-        // $this->assertFileExists('/path/to/file');
+        $urls = ['https://www.aksa.rs/osnovni-modeli-kolica/1585-graco-kolica-evo' , 'https://www.junglebaby.rs/kolica-od-0m/8933-bugaboo-decija-kolica-bee-6' , 
+        'https://www.aksa.rs/posteljine/22277-lillopippo-punjena-posteljina-slatkisi', 'https://www.aksa.rs/prekrivaci-jorgani-i-cebad/20952-chicco-cebe-unisex'];
+        $outputs = [
+            [
+                "domain" => 'aksa',
+                "url" => 'www.aksa.rs',
+                "data" => [
+                "price" => 'document.querySelector("div.block.product-details-price > div.clearfix > div > span.product-price-value.value").innerText'
+                ]
+            ],
+            
+        ];
+        $craw->bulkImport('https://www.aksa.rs/osnovni-modeli-kolica/1585-graco-kolica-evo',$outputs);
+        print_r($craw->getBulk());
     }
 
 }
